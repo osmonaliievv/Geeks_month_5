312a13284836089f92fc4d400d4d8b9bb306850a
Adilnur1 <osmonaliievv@gmail.com>
2025-02-05 19:16:08 +0600

    first commit

---
+++ movie_app/views.py
@@ -0,0 +1,59 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from movie_app.models import Director, Movie, Review
+from movie_app.serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
+from rest_framework import status
+
+
+@api_view(['GET'])
+def director_detail_api_view(request, id):
+    try:
+        director = Director.objects.get(id=id)
+    except Director.DoesNotExist:
+        return Response(data={'error': 'Director not found'}, status=status.HTTP_404_NOT_FOUND)
+    data = DirectorSerializer(director).data
+    return Response(data=data)
+
+
+@api_view(http_method_names=['GET'])
+def director_list_api_view(request):
+    directors = Director.objects.all()
+    data = DirectorSerializer(directors, many=True).data
+    print(data)
+    return Response(data={'directors': data})
+
+
+@api_view(['GET'])
+def movie_detail_api_view(request, id):
+    try:
+        movie = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(data={'error': 'Director not found'}, status=status.HTTP_404_NOT_FOUND)
+    data = MovieSerializer(movie).data
+    return Response(data=data)
+
+
+@api_view(http_method_names=['GET'])
+def movie_list_api_view(request):
+    movies = Movie.objects.all()
+    data = MovieSerializer(movies, many=True).data
+    print(data)
+    return Response(data={'movies': data})
+
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(data={'error': 'Director not found'}, status=status.HTTP_404_NOT_FOUND)
+    data = ReviewSerializer(review).data
+    return Response(data=data)
+
+
+@api_view(http_method_names=['GET'])
+def review_list_api_view(request):
+    reviews = Review.objects.all()
+    data = ReviewSerializer(reviews, many=True).data
+    print(data)
+    return Response(data={'reviews': data})
